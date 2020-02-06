114064c34aa5df4a2711a5241be4c86f10779a84
Dahan Schuster <dan.plschuster@gmail.com>
2020-02-06 09:35:26 -0300

    Limpa o index.php para ser reescrito
    O index.php será o local de aplicação das classes e interfaces criadas
    Deve seguir os padrões de codificação adotados na biblioteca e,
    portanto, foi limpo para poder ser reescrito seguindo os padrões

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
 <?php
 
-require './vendor/autoload.php';
-
-use App\Repositories\TagRepository;
-use App\Repositories\LeadRepository;
-use App\Repositories\UserRepository;
-use App\Http\Controller\UserController;
-
-$container = new App\Container();
-
-$container->set(TagRepository::class, function() {
-    return new TagRepository();
-});
-
-$container->set(LeadRepository::class, function() {
-    return new LeadRepository();
-});
-
-$container->set(UserRepository::class, function() {
-    return new UserRepository();
-});
-
-/** @var UserController $userController */
-$userController = $container->get(UserController::class);
-$userController->index();
+echo "Aqui será orquestrada a demonstração do Container de Dependências.";
